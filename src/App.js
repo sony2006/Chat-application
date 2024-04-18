ecbd02fa8b3a9d81b396fc73292d9c411e2645bd
sony2006 <sonyyadav7386@gmail.com>
2024-04-18 15:41:45 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const projectID = 'f22828ef-16a3-4db4-99b7-38e7112c7bc3';
+  if (!localStorage.getItem('username')) return <LoginForm/>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName="Sony"
+      userSecret="7386"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps }/>}
+    />
   );
 }
 
